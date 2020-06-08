b3fc88bbb47b95a26880f2765b6144625388eb3c
Jamila90 <jamila.shakurova@gmail.com>
2020-06-08 21:01:53 +0400

    page layout

---
+++ build/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mainNav = document.querySelector('.main-nav');
+var toggleBtn = document.querySelector('.main-header__toggle');
+
+
+mainNav.classList.remove('main-nav--nojs');
+
+toggleBtn.addEventListener('click', function () {
+  mainNav.classList.toggle('main-nav--closed');
+});
+
+
+toggleBtn.addEventListener('click', function () {
+  toggleBtn.classList.toggle('main-header__toggle--closed');
+});
