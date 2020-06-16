bb0a9f4ea35034f705ea811a3acb6a3d8e9a3b2b
Jamila90 <jamila.shakurova@gmail.com>
2020-06-16 16:46:30 +0400

    'noname'

--- build/js/script.js
+++ build/js/script.js
@@ -5,11 +5,11 @@ var toggleBtn = document.querySelector('.main-header__toggle');
 
 
 mainNav.classList.remove('main-nav--nojs');
+toggleBtn.classList.remove('main-header__toggle--nojs');
 
 toggleBtn.addEventListener('click', function () {
   mainNav.classList.toggle('main-nav--closed');
 });
-
 
 toggleBtn.addEventListener('click', function () {
   toggleBtn.classList.toggle('main-header__toggle--closed');
