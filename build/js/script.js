8a660d220ef04999f093602aab23661f3764db2b
Jamila90 <jamila.shakurova@gmail.com>
2020-06-20 08:53:39 +0400

    'bugsEdit'

--- build/js/script.js
+++ build/js/script.js
@@ -1,11 +1,12 @@
 'use strict';
 
+var mainHeader = document.querySelector('.main-header');
 var mainNav = document.querySelector('.main-nav');
 var toggleBtn = document.querySelector('.main-header__toggle');
 
-
 mainNav.classList.remove('main-nav--nojs');
 toggleBtn.classList.remove('main-header__toggle--nojs');
+mainHeader.classList.remove('main-header--nojs');
 
 toggleBtn.addEventListener('click', function () {
   mainNav.classList.toggle('main-nav--closed');
