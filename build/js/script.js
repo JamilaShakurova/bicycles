100b157f4263ea78d2edd16c89d61fc0a166fb76
Jamila90 <jamila.shakurova@gmail.com>
2020-06-23 21:49:36 +0400

    'bugFixes'

--- build/js/script.js
+++ build/js/script.js
@@ -1,17 +1,23 @@
 'use strict';
 
-var mainHeader = document.querySelector('.main-header');
-var mainNav = document.querySelector('.main-nav');
-var toggleBtn = document.querySelector('.main-header__toggle');
+let mainHeader = document.querySelector(`.main-header`);
+let mainNav = document.querySelector(`.main-nav`);
+let toggleBtn = document.querySelector(`.main-header__toggle`);
 
-mainNav.classList.remove('main-nav--nojs');
-toggleBtn.classList.remove('main-header__toggle--nojs');
-mainHeader.classList.remove('main-header--nojs');
 
-toggleBtn.addEventListener('click', function () {
-  mainNav.classList.toggle('main-nav--closed');
+mainNav.classList.remove(`main-nav--nojs`);
+toggleBtn.classList.remove(`main-header__toggle--nojs`);
+mainHeader.classList.remove(`main-header--nojs`);
+
+toggleBtn.addEventListener(`click`, function () {
+  mainNav.classList.toggle(`main-nav--closed`);
 });
 
-toggleBtn.addEventListener('click', function () {
-  toggleBtn.classList.toggle('main-header__toggle--closed');
+toggleBtn.addEventListener(`click`, function () {
+  toggleBtn.classList.toggle(`main-header__toggle--closed`);
 });
+
+  $(document).ready(function () {
+    $('#phone').mask("+7(999) 999-99-99");
+  });
+
